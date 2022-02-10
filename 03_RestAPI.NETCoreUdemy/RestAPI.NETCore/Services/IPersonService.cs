e9ede9b714e09019b6ba7d27f83d2c7afb148742
Gabriel Alecio <aleciorpo@gmail.com>
2022-02-10 20:47:25 -0300

    Persons and Data Base

---
+++ 03_RestAPI.NETCoreUdemy/RestAPI.NETCore/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestAPI.NETCore.Model;
+
+namespace RestAPI.NETCore.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
